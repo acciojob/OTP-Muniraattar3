9d928fb28e386b0571d18796f483dbe5ced9fe25
Munira Yusuf Ali Attar <124035031+Muniraattar3@users.noreply.github.com>
2025-01-04 18:33:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+inputs.forEach(input, index)=>{
+	inputs.addEventListner('input',()=>{
+		if(input.value !==''){
+			if(index<inputs.lenght-1){
+				inputs[index+1].focus();
+			}
+		}
+	});
+	input.addEventListner('keydon',(e)=>{
+		if(e.key==='Backspace'&& index>0 && input.value===''){
+			inputs[index-1].focus();
+		}
+	});
+});
+inputs[0].focus();
